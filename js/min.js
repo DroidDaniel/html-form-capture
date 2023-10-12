2bebf240131eddd6050d54cc70e9a37692b62e73
Sri <sriar.srivati@ansira.com>
2023-10-12 13:42:43 -0700

    initial val

--- js/min.js
+++ js/min.js
@@ -1,16 +1,6 @@
 $(document).ready(function () {
-  var $form = $("form");
-  $form.submit(function () {
-    $.post($(this).attr("action"), $(this).serialize(), function (response) {
-      console.log("submited");
-    });
-    $("#sp_form")[0].reset();
-    return false;
-  });
-});
-
-$(document).ready(function () {
-  $("#btn_submit").click(function () {
+  $("#btn_submit").click(function (e) {
+    e.preventDefault();
     if ($("#name").val().length == 0) {
       $("#name").addClass("highlight");
       $(".error_msg").show();
@@ -19,91 +9,20 @@ $(document).ready(function () {
       $("#name").removeClass("highlight");
       $(".error_msg").hide();
       $(".success_msg").show();
-    }
-    if ($("#mobile").val().length == 0) {
-      $("#mobile").addClass("highlight");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    } else {
-      $("#mobile").removeClass("highlight");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    }
-    // if ($("#email").val().length == 0) {
-    //   $("#email").addClass("highlight");
-    //   $(".error_msg").show();
-    //   $(".success_msg").hide();
-    // } else {
-    //   $("#email").removeClass("highlight");
-    //   $(".error_msg").hide();
-    //   $(".success_msg").show();
-    // }
-    var email = $("#email").val();
-    var regex =
-      /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!regex.test(email)) {
-      $("#email").addClass("highlight");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    } else {
-      $("#email").removeClass("highlight");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    }
-
-    if ($("#rooms").val().length == 0) {
-      $("#rooms").addClass("highlight");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    } else {
-      $("#rooms").removeClass("highlight");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    }
-    if ($("#adults").val().length == 0) {
-      $("#adults").addClass("highlight");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    } else {
-      $("#adults").removeClass("highlight");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    }
-    if ($("#kids").val().length == 0) {
-      $("#kids").addClass("highlight");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    } else {
-      $("#kids").removeClass("highlight");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    }
-    if (!$("#property").val()) {
-      $("#property").addClass("highlight-select");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    } else {
-      $("#property").removeClass("highlight-select");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    }
-    if (!$("#checkin").val().length == 0) {
-      $("#checkin").removeClass("highlight-date");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    } else {
-      $("#checkin").addClass("highlight-date");
-      $(".error_msg").show();
-      $(".success_msg").hide();
-    }
-    if (!$("#checkout").val().length == 0) {
-      $("#checkout").removeClass("highlight-date");
-      $(".error_msg").hide();
-      $(".success_msg").show();
-    } else {
-      $("#checkout").addClass("highlight-date");
-      $(".error_msg").show();
-      $(".success_msg").hide();
+      // $("#sp_form").submit(function (e) {
+      $.ajax({
+        url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeMH7qH5GCExICI52qYs_r56eG4F2RfQjCTVB7YbQBZxHjtkA/formResponse",
+        data: $(this).serialize(),
+        type: "POST",
+        dataType: "xml",
+        success: function (data) {
+          console.log("Submission successful");
+        },
+        error: function (xhr, status, error) {
+          console.log("Submission failed: " + error);
+        },
+      });
+      // });
     }
   });
 });
